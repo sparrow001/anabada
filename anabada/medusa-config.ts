ffec4583c9a9ab739d0735dab7c9865688006533
Sparrow001 <66333207+sparrow001@users.noreply.github.com>
2024-12-12 16:29:38 -0500

    maybe add redis support?

--- anabada/medusa-config.ts
+++ anabada/medusa-config.ts
@@ -5,6 +5,8 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL ||
+       undefined,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
